d283707fe0bc9c212c8ec406b94337ecac9f4eff
Mustafa <cana66331@gmail.com>
2020-07-30 16:24:32 +0200

    backend modifiyed

---
+++ backend/build/precache-manifest.648a10617637b5501d735d5250f0f1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f9ddda508cd6af7ad2d2cb410a3d24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d7b6df8dbc36e07b1da",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d7b6df8dbc36e07b1da",
+    "url": "/static/js/main.8b4371a0.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
